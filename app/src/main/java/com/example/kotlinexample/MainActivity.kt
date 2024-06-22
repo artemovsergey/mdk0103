4fc95fbb855d112233a29a7399596da3992230ef
artemov <artik3314@gmail.com>
2024-06-22 11:37:09 +0300

    Init

---
+++ app/src/main/java/com/example/kotlinexample/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.kotlinexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kotlinexample.ui.theme.KotlinExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+           UserInfo(name = "Сергей", age = 35 )
+        }
+    }
+}
+
+@Composable
+fun UserInfo(name: String, age: Int) {
+    Text(
+        text = "Здравствуйте,  $name. Ваш возраст:  $age лет"
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    UserInfo(name = "Сергей", age = 35 )
+}
