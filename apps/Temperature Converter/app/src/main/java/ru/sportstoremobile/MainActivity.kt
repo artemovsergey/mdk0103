c1e68bbfbee9886b19c763afc38659f947f4a326
artemov <artik3314@gmail.com>
2024-10-28 21:13:17 +0300

    .

---
+++ apps/Temperature Converter/app/src/main/java/ru/sportstoremobile/MainActivity.kt
@@ -0,0 +1,131 @@
+package ru.sportstoremobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+            MaterialTheme {
+                Surface {
+                    MainActivityContent()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Header(image: Int, description: String) {
+    Image(
+        painter = painterResource(image),
+        contentDescription = description,
+        modifier = Modifier.height(180.dp).fillMaxWidth(),
+        contentScale = ContentScale.Crop
+    )
+}
+
+@Composable
+fun TemperatureText(celsius: Int) {
+    val fahrenheit = (celsius.toDouble()*9/5)+32
+    Text("$celsius Celsius is $fahrenheit Fahrenheit")
+}
+
+@Composable
+fun ConvertButton(clicked: () -> Unit) {
+    Button(onClick = clicked) {
+        Text("Convert")
+    }
+}
+
+@Composable
+fun ExampleTextField() {
+    val text = remember { mutableStateOf("") }
+    TextField(
+        value = text.value,
+        onValueChange = { text.value = it },
+        label = { Text("What is your name?") }
+    )
+}
+
+@Composable
+fun EnterTemperature(temperature: String, changed: (String) -> Unit) {
+    TextField(
+        value = temperature,
+        label = { Text("Enter a temperature in Celsius") },
+        onValueChange = changed,
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+fun MainActivityContent(){
+
+    val celsius = remember { mutableStateOf(0) }
+    val newCelsius = remember { mutableStateOf("") }
+
+    Column(modifier = Modifier.padding(15.dp),horizontalAlignment = Alignment.CenterHorizontally){
+        Header(R.drawable.sunrise, "sunrise image")
+        EnterTemperature(newCelsius.value) { newCelsius.value = it }
+        ConvertButton {
+            newCelsius.value.toIntOrNull()?.let {
+                celsius.value = it
+            }
+        }
+        TemperatureText(celsius.value)
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MainActivityContentPreview() {
+    MainActivityContent()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
