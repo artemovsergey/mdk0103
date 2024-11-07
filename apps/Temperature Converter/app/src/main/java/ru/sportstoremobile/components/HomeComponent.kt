d6db9c761b5b4613b5d9c68bd39060ede93fa7db
artemov <artik3314@gmail.com>
2024-11-07 08:31:15 +0300

    .

---
+++ apps/Temperature Converter/app/src/main/java/ru/sportstoremobile/components/HomeComponent.kt
@@ -0,0 +1,9 @@
+package ru.sportstoremobile.components
+
+@androidx.compose.runtime.Composable
+fun (modifier: androidx.compose.ui.Modifier = androidx.compose.ui.Modifier) {
+    
+}
+
+class HomeComponent {
+}
