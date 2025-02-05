b0abfa092510838ef85f058c343c2b77a5d3ae9a
artemov <artik3314@gmail.com>
2025-02-05 23:00:09 +0300

    .

---
+++ TestPorject.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using TestPorject.Console;
+using System;
+
+public static class Program{
+    public static void Main(){
+
+        var user = new User(1, "bob");
+        var userRecord = new UserRecord(1, "bob");
+        Console.WriteLine(userRecord.Name);
+        System.Console.WriteLine(user.Name);
+    }
+}
